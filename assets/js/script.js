12ecc76ef0d3137852402159fe99e0d20e7e167e
kate <kan004@ucr.edu>
2020-05-20 21:13:37 -0700

    added password criteria prompts

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  // you can create a function named generatePassword that creates the password
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+  prompt("include upper case letters?");
+  prompt("include lower case letters?");
+  prompt("include special characters?");
+  prompt("include numbers?");
+  prompt("password length? (8-128)");
+
+}
+
+
+
+function generatePassword(){
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var specialChar = ["\"","!","#","$","%","&","\'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
+  var num = ["0","1","2","3","4","5","6","7","8","9"];
+
+
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+

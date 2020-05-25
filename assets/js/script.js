a3aeea69d09ccfe68e881f5db4579cebb7994994
kate <kan004@ucr.edu>
2020-05-25 16:00:14 -0700

    taking chosen password criteria and pushing into new variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ function generatePassword(){
   var number = confirm("include numbers?");
 
 // validating input and ensuring at least one char type is included
-  while ((upper===false) && (lower===false) && (special===false) && (number===false)) {
+  while((upper===false) && (lower===false) && (special===false) && (number===false)) {
   alert("Please choose at least one type of character!");
   var upper = confirm("include upper case letters?");
   var lower = confirm("include lower case letters?");
@@ -35,7 +35,7 @@ function generatePassword(){
 
 // prompting user for password length and ensuring length is between 8-128
   var userLength = prompt("How long would you like your password to be? (8-128)");
-  while ((userLength < 8) || (userLength > 128)){
+  while((userLength < 8) || (userLength > 128)){
     alert("Please choose a length between 8 and 128");
     prompt("How long would you like your password to be? (8-128)");
   }
@@ -45,6 +45,36 @@ function generatePassword(){
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var specialChar = ["\"","!","#","$","%","&","\'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
   var num = ["0","1","2","3","4","5","6","7","8","9"];
+
+// if uppercase letters are included, go through upperCase array and push into chosen
+if(upper){
+    for(var i=0; i < upperCase.length; i++){
+      chosen.push(upperCase[i]);
+    }
+}
+
+// if lowerCase letters are included, go through lowerCase array and push into chosen
+if(lower){
+  for(var j=0; j < lowerCase.length; j++){
+    chosen.push(lowerCase[j]);
+  }
+}
+
+// if speical characters are included, go through specialChar array and push into chosen
+if(special){
+  for(var k=0; k < speicalChar.length; k++){
+    chosen.push(specialChar[k]);
+  }
+}
+
+// if numbers are included, go through num array and push into chosen
+if(number){
+  for(var l=0; l < num.length; l++){
+    chosen.push(num[l]);
+  }
+}
+
+console.log(chosen.join(""));
 
 }
 
